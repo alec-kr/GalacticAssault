68b8ead4c79d7839f5cb3c3d8d99958b8d76499b
alec-kr <akramdhan02@gmail.com>
2023-02-06 15:35:56 -0400

    Added popup settings menu

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -5,9 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
+    private GameObject settingsPanel;
+
+    private void Start() {
+        settingsPanel = GameObject.Find("Panel").transform.Find("SettingsPanel").gameObject;
+    }
     // public int gameScene;
     public void playGame() {
         // SceneManager.LoadScene(gameScene);
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void showSettings() {
+        settingsPanel.SetActive(true);
+    }
+
+    public void hideSettings() {
+        settingsPanel.SetActive(false);
     }
 }
